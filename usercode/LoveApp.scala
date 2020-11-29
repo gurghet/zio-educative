b7bde751f190a5d4d7c947bc2c865d9d8e42bf8b
Andrea Passaglia <gurghet@gmail.com>
2020-11-29 18:50:27 +0000

    remove bets add love app

--- usercode/LoveApp.scala
+++ usercode/LoveApp.scala
@@ -1,30 +1,51 @@
-import predictor.Matcher
+import java.io.IOException
+
+import CrystalGazer.CrystalGazer
+import Match.Match
+import zio._
 import zio.console.{ Console, getStrLn, putStr, putStrLn }
 import zio.macros.accessible
-import zio.{ ExitCode, Has, UIO, ULayer, URIO, ZIO, ZLayer }
 
-object MatchCalculator extends zio.App {
-  val program: ZIO[Console with Matcher, Serializable, Unit] = for {
-    name1  <- putStr("Enter your name: ") *> getStrLn
-    name2  <- putStr("Enter your crush name: ") *> getStrLn
-    result <- Matcher.computeMatch(name1, name2)
-    _      <- putStrLn(s"You are a $result% match!")
-  } yield ()
+object LoveApp extends zio.App {
+  val program: ZIO[Console with Match with CrystalGazer, IOException, Unit] =
+    for {
+      name1   <- putStr("Enter your name: ") *> getStrLn
+      name2   <- putStr("Enter your crush name: ") *> getStrLn
+      result  <- Match.computeMatch(name1, name2)
+      fortune <- CrystalGazer.gaze(name1, name2)
+      _       <- putStrLn(s"You are a $result% match!")
+      _       <- putStrLn(fortune)
+    } yield ()
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
-    program.provideCustomLayer(Matcher.live).exitCode
+    program.provideCustomLayer(Match.live ++ CrystalGazer.live).exitCode
 }
 
-object predictor {
-  type Matcher = Has[Matcher.Service]
+@accessible
+object Match {
+  type Match = Has[Match.Service]
 
-  @accessible
-  object Matcher {
-    trait Service {
-      def computeMatch(name1: String, name2: String): UIO[Int]
-    }
-
-    val live: ULayer[Matcher] =
-      ZLayer.succeed((name1: String, name2: String) => UIO(100))
+  trait Service {
+    def computeMatch(name1: String, name2: String): UIO[Int]
   }
+
+  val live: ULayer[Match] =
+    ZLayer.succeed((name1: String, name2: String) => UIO(100))
+}
+
+@accessible
+object CrystalGazer {
+  type CrystalGazer = Has[CrystalGazer.Service]
+
+  trait Service {
+    def gaze(name1: String, name2: String): UIO[String]
+  }
+
+  val live: ULayer[CrystalGazer] =
+    ZLayer.succeed((name1: String, name2: String) =>
+      UIO {
+        val childrenCount = (name1.length + name2.length) / 4
+        s"You will have $childrenCount children."
+      }
+    )
 }
