dd42132d7dccfad8a82c6967ce3bee09b05a98a5
Andrea Passaglia <gurghet@gmail.com>
2021-01-17 21:51:12 +0000

    add coffee machine and word counter

--- usercode/CoffeeMachine.scala
+++ usercode/CoffeeMachine.scala
@@ -1,8 +1,18 @@
-import zio.{ App, ExitCode, URIO }
-object Nucular extends zio.App {
-  val enrichUranium: Unit                                        = println("enriching uranium")
-  val miningUranium: Unit                                        = println("mining uranium")
-  val reachCritical: Unit                                        = println("uranium reached critical mass")
-  def nuke(target: String): Unit                                 = println(s"nuking $target")
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = ???
+class Cappuccino(var coffee: Int, var milk: Int)
+object CoffeeMachine extends App {
+  def pourCoffee(cappuccino: Cappuccino): Unit = cappuccino.coffee += 1
+  def pourMilk(cappuccino: Cappuccino): Unit = cappuccino.milk +=
+    cappuccino.coffee * 5
+
+  // recipe
+  // get a new cappuccino
+  val cappuccino = new Cappuccino(0, 0)
+  // pour one part of coffee
+  pourCoffee(cappuccino)
+  // pour five parts of milk
+  pourMilk(cappuccino)
+  // your cappuccino is ready!
+  println(
+    s"Your cappuccino is ${cappuccino.coffee} parts coffee and ${cappuccino.milk} parts milk"
+  )
 }
--- usercode/WordCounter.scala
+++ usercode/WordCounter.scala
@@ -1,3 +1,27 @@
-object WordCounter {
+import zio.console.putStrLn
+import zio.stream.ZStream
+import zio.{ ExitCode, UIO, URIO }
 
+object WordCounter extends zio.App {
+
+  val poem: String =
+    """Never trust atoms.
+      |They make up everything.""".stripMargin
+
+  case class Accumulator(count: Long, prevChar: Char)
+
+  val stream: UIO[Long] = ZStream
+    .fromIterable(poem)
+    .mapAccum(Accumulator(0, ' ')) { (acc, char) =>
+      val newAcc =
+        if (char.isLetterOrDigit && !acc.prevChar.isLetterOrDigit)
+          Accumulator(acc.count + 1, char)
+        else Accumulator(acc.count, char)
+      (newAcc, newAcc.count)
+    }
+    .runLast
+    .map(_.getOrElse(0))
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    stream.flatMap(s => putStrLn(s.toString)).exitCode
 }
